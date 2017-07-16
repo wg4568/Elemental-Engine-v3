cb2c80b9aad771fee48587ba13c44aed7a13faee
William Gardner <wg4568@gmail.com>
2017-07-15 22:06:17 -0700

    Added back main functionality

--- elemental.js
+++ elemental.js
@@ -1,10 +1,216 @@
 var Elemental = {};
 
+Elemental.Input = class {
+	static get MousePos() {
+		return Elemental.Backend.mousePos;
+	}
+
+	static KeyHeld(keycode) {
+		var val = Elemental.Backend.keyStates[keycode];
+		if (val == 1) { return true; }
+		else { return false; }
+	}
+
+	static KeyPressed(keycode) {
+		var val = Elemental.Backend.keysDown[keycode];
+		if (val == 1) { return true; }
+		else { return false; }
+	}
+
+	static KeyReleased(keycode) {
+		var val = Elemental.Backend.keysUp[keycode];
+		if (val == 1) { return true; }
+		else { return false; }
+	}
+
+	static MouseHeld(button) {
+		var val = Elemental.Backend.mouseState[button];
+		if (val == 1) { return true; }
+		else { return false; }
+	}
+
+	static MousePressed(button) {
+		var val = Elemental.Backend.mouseDown[button];
+		if (val == 1) { return true; }
+		else { return false; }
+	}
+
+	static MouseReleased(button) {
+		var val = Elemental.Backend.mouseUp[button];
+		if (val == 1) { return true; }
+		else { return false; }
+	}
+}
+
+Elemental.Backend = {
+	mousePos: {x: 0, y: 0},
+	keyStates: {},
+	keysDown: {},
+	keysUp: {},
+	mouseState: {},
+	mouseDown: {},
+	mouseUp: {},
+	spinoffs: [],
+	spinoffNames: []
+}
+
+Elemental.Spinoff = class {
+	constructor(func, length, name="") {
+		this.func = func;
+		this.length = length;
+		this.frame = 0;
+		this.name = name;
+	}
+
+	doFrame() {
+		this.func(this.frame);
+		this.frame++;
+		if (this.frame > this.length) {
+			Elemental.Spinoff.Kill(this);
+		}
+	}
+
+	static Start(func, length, id="") {
+		if (Elemental.Backend.spinoffNames.indexOf(id) == -1) {
+			var so = new Elemental.Spinoff(func, length, name=id);
+			Elemental.Backend.spinoffs.push(so);
+			if (so.name != "") {
+				Elemental.Backend.spinoffNames.push(so.name);
+			}
+			return so;
+		}
+	}
+
+	static Kill(spinoff) {
+		var index = Elemental.Backend.spinoffs.indexOf(spinoff);
+		if (index != -1) {
+ 			Elemental.Backend.spinoffs.splice(index, 1);
+		}
+		var index2 = Elemental.Backend.spinoffNames.indexOf(spinoff.name);
+		if (index2 != -1) {
+ 			Elemental.Backend.spinoffNames.splice(index2, 1);
+		}
+	}
+
+	static RunAll() {
+		Elemental.Backend.spinoffs.forEach(function(so){
+			so.doFrame();
+		});
+	}
+}
+
+// GameLoopManager By Javier Arevalo
+Elemental.Backend.Timer = new function() {
+	this.lastTime = 0;
+	this.gameTick = null;
+	this.prevElapsed = 0;
+	this.prevElapsed2 = 0;
+
+	this.start = function(gameTick) {
+		var prevTick = this.gameTick;
+		this.gameTick = gameTick;
+		if (this.lastTime == 0)
+		{
+			// Once started, the loop never stops.
+			// But this function is called to change tick functions.
+			// Avoid requesting multiple frames per frame.
+			var bindThis = this;
+			requestAnimationFrame(function() { bindThis.tick(); } );
+			this.lastTime = 0;
+		}
+	}
+
+	this.stop = function() {
+		this.start (null);
+	}
+
+	this.tick = function () {
+		if (this.gameTick != null)
+		{
+			var bindThis = this;
+			requestAnimationFrame(function() { bindThis.tick(); } );
+		}
+		else
+		{
+			this.lastTime = 0;
+			return;
+		}
+		var timeNow = Date.now();
+		var elapsed = timeNow - this.lastTime;
+		if (elapsed > 0)
+		{
+			if (this.lastTime != 0)
+			{
+				if (elapsed > 1000) // Cap max elapsed time to 1 second to avoid death spiral
+				elapsed = 1000;
+				// Hackish fps smoothing
+				var smoothElapsed = (elapsed + this.prevElapsed + this.prevElapsed2)/3;
+				this.gameTick(0.001*smoothElapsed);
+				this.prevElapsed2 = this.prevElapsed;
+				this.prevElapsed = elapsed;
+			}
+			this.lastTime = timeNow;
+		}
+	}
+}
+
+Elemental.Start = function(func) {
+	Elemental.Backend.Timer.start(function(time){
+		func(time);
+		Elemental.Spinoff.RunAll();
+		Elemental.Backend.ResetKeys();
+	});
+}
+
+Elemental.Stop = function() {
+	Elemental.Backend.Timer.stop();
+}
+
+Elemental.Backend.ResetKeys = function() {
+	Elemental.Backend.keysDown = {};
+	Elemental.Backend.keysUp = {};
+	Elemental.Backend.mouseDown = {};
+	Elemental.Backend.mouseUp = {};
+}
+
+document.addEventListener("keydown", function(event) {
+	if (!Elemental.Input.KeyHeld(event.keyCode)) {
+		Elemental.Backend.keysDown[event.keyCode] = 1;
+	}
+	Elemental.Backend.keyStates[event.keyCode] = 1;
+});
+
+document.addEventListener("keyup", function(event) {
+	Elemental.Backend.keyStates[event.keyCode] = 0;
+	Elemental.Backend.keysUp[event.keyCode] = 1;
+});
+
+document.addEventListener("mousedown", function(event) {
+	if (!Elemental.Input.MouseHeld(event.button)) {
+		Elemental.Backend.mouseDown[event.button] = 1;
+	}
+	Elemental.Backend.mouseState[event.button] = 1;
+});
+
+document.addEventListener("mouseup", function(event) {
+	Elemental.Backend.mouseState[event.button] = 0;
+	Elemental.Backend.mouseUp[event.button] = 1;
+});
+
 Elemental.Canvas = class {
 	constructor(id, fullscreen=false) {
 		this.canvas = document.getElementById(id);
 		this.context = this.canvas.getContext("2d");
 		this.fullscreen = fullscreen;
+
+		this.mousePos = Elemental.Vector.Empty;
+
+		this.canvas.addEventListener("contextmenu", event => event.preventDefault());
+
+		var parent = this;
+		this.canvas.addEventListener("mousemove", function(event) {
+			Elemental.Backend.mousePos = new Elemental.Vector(event.offsetX, event.offsetY);
+		});
 
 		if (this.fullscreen) {
 			this.fillWindow();
@@ -59,6 +265,10 @@ Elemental.Canvas = class {
 
 	drawImage(image, posn, scale=1) {
 		this.context.drawImage(image, posn.x, posn.y, image.width*scale, image.height*scale);
+	}
+
+	drawSprite(sprite, posn) {
+		sprite.drawOnCanvas(this, posn);
 	}
 }
 
@@ -127,7 +337,7 @@ Elemental.Sprite.Points = class extends Elemental.Sprite {
 			(this.points[0].y-this.center.y)*this.scale
 		);
 		for (var i=1; i<this.points.length; i++) {
-				canvas.context.lineTo(
+			canvas.context.lineTo(
 				(this.points[i].x-this.center.x)*this.scale,
 				(this.points[i].y-this.center.y)*this.scale
 			);
@@ -145,7 +355,7 @@ Elemental.Sprite.Points = class extends Elemental.Sprite {
 			if (this.lineWidth > 0) { canvas.context.stroke(); }
 		}
 
-		canvas.context.globalAlpha = 1;
+		canvas.context.globalAlpha = 1;0
 		canvas.context.rotate(-Elemental.Helpers.ToRadians(this.rotation));
 		canvas.context.translate(-posn.x, -posn.y);
 	}
@@ -166,7 +376,7 @@ Elemental.Sprite.Polygon = class extends Elemental.Sprite {
 		this.lineDashWidth = null;
 		this.lineDashSpacing = null;
 
-		this.fillColor = null;
+		this.fillColor = "white";
 		this.strokeFirst = false;
 
 		this.inherit(config);
@@ -189,7 +399,7 @@ Elemental.Sprite.Polygon = class extends Elemental.Sprite {
 		canvas.context.beginPath();
 		canvas.context.moveTo(
 			(this.size-this.center.x)*this.scale,
-			this.center.y*this.scale
+			-this.center.y*this.scale
 		);
 
 		for (var angle = 360/this.sides; angle < 360; angle += 360/this.sides) {
@@ -419,61 +629,6 @@ Elemental.Helpers.Now = function() {
 	return new Date().getTime() / 1000;
 }
 
-// GameLoopManager By Javier Arevalo
-Elemental.Helpers.GameLoopManager = new function() {
-	this.lastTime = 0;
-	this.gameTick = null;
-	this.prevElapsed = 0;
-	this.prevElapsed2 = 0;
-
-	this.run = function(gameTick) {
-		var prevTick = this.gameTick;
-		this.gameTick = gameTick;
-		if (this.lastTime == 0)
-		{
-			// Once started, the loop never stops.
-			// But this function is called to change tick functions.
-			// Avoid requesting multiple frames per frame.
-			var bindThis = this;
-			requestAnimationFrame(function() { bindThis.tick(); } );
-			this.lastTime = 0;
-		}
-	}
-
-	this.stop = function() {
-		this.run(null);
-	}
-
-	this.tick = function () {
-		if (this.gameTick != null)
-		{
-			var bindThis = this;
-			requestAnimationFrame(function() { bindThis.tick(); } );
-		}
-		else
-		{
-			this.lastTime = 0;
-			return;
-		}
-		var timeNow = Date.now();
-		var elapsed = timeNow - this.lastTime;
-		if (elapsed > 0)
-		{
-			if (this.lastTime != 0)
-			{
-				if (elapsed > 1000) // Cap max elapsed time to 1 second to avoid death spiral
-				elapsed = 1000;
-				// Hackish fps smoothing
-				var smoothElapsed = (elapsed + this.prevElapsed + this.prevElapsed2)/3;
-				this.gameTick(0.001*smoothElapsed);
-				this.prevElapsed2 = this.prevElapsed;
-				this.prevElapsed = elapsed;
-			}
-			this.lastTime = timeNow;
-		}
-	}
-}
-
 // Vector class and function definitions
 Elemental.Vector = class {
 	constructor(x, y) {
@@ -548,4 +703,113 @@ Elemental.Vector = class {
 		}
 		return total;
 	}
+}
+
+// Mouse and keycode definitions
+Elemental.Keycodes = {
+	BACKSPACE: 8,
+	TAB: 9,
+	ENTER: 13,
+	SHIFT: 16,
+	CTRL: 17,
+	ALT: 18,
+	BREAK: 19,
+	CAPSLOCK: 20,
+	ESCAPE: 27,
+	SPACE: 32,
+	PGUP: 33,
+	PGDOWN: 34,
+	END: 35,
+	HOME: 36,
+	LEFT: 37,
+	UP: 38,
+	RIGHT: 39,
+	DOWN: 40,
+	INSERT: 45,
+	DELETE: 46,
+	N0: 48,
+	N1: 49,
+	N2: 50,
+	N3: 51,
+	N4: 52,
+	N5: 53,
+	N6: 54,
+	N7: 55,
+	N8: 56,
+	N9: 57,
+	A: 65,
+	B: 66,
+	C: 67,
+	D: 68,
+	E: 69,
+	F: 70,
+	G: 71,
+	H: 72,
+	I: 73,
+	J: 74,
+	K: 75,
+	L: 76,
+	M: 77,
+	N: 78,
+	O: 79,
+	P: 80,
+	Q: 81,
+	R: 82,
+	S: 83,
+	T: 84,
+	U: 85,
+	V: 86,
+	W: 87,
+	X: 88,
+	Y: 89,
+	Z: 90,
+	LWIN: 91,
+	RWIN: 92,
+	SELECT: 93,
+	NUM0: 96,
+	NUM1: 97,
+	NUM2: 98,
+	NUM3: 99,
+	NUM4: 100,
+	NUM5: 101,
+	NUM6: 102,
+	NUM7: 103,
+	NUM8: 104,
+	NUM9: 105,
+	MULTIPLY: 106,
+	ADD: 107,
+	SUBTRACT: 109,
+	PERIOD: 110,
+	DIVIDE: 111,
+	F1: 112,
+	F2: 113,
+	F3: 114,
+	F4: 115,
+	F5: 116,
+	F6: 117,
+	F7: 118,
+	F8: 119,
+	F9: 120,
+	F10: 121,
+	F11: 122,
+	F12: 123,
+	NUMLOCK: 144,
+	SCROLLLOCK: 145,
+	SEMICOLON: 186,
+	EQUAL: 187,
+	COMMA: 188,
+	DASH: 189,
+	PERIOD: 190,
+	FSLASH: 191,
+	GRAVE: 192,
+	OBRACKET: 219,
+	BSLASH: 220,
+	CBRACKET: 221,
+	QUOTE: 222
+}
+
+Elemental.Mousecodes = {
+	LEFT: 0,
+	MIDDLE: 1,
+	RIGHT: 2
 }
