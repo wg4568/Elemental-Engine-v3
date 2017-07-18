834f79528453fb41a83372d76c59f191fb4251e0
William Gardner <wg4568@gmail.com>
2017-07-18 12:03:41 -0700

    wrote simple tank-multiplayer server

--- server/server_test.js
+++ server/server_test.js
@@ -1,27 +1,61 @@
+// Import server code
 const Elemental = require("./server.js");
 
-var server = new Elemental.Server(8080);
+// Set up server on port 80, at 60 ticks/second
+var server = new Elemental.Server(8080, tickrate=60);
 
+// Define target movespeed
+var movespeed = 5;
+
+// Called when client connects
 server.onConnect = function(client) {
+	// Set color and position
+	client.color = Elemental.Helpers.RandomColor();
+	client.tankpos = Elemental.Vector.Empty;
+
+	// Send some basic information to the client
+	client.callTrigger("configure", {id: client.id, color: client.color.formatHEX(), movespeed: movespeed});
+
+	// Print a console alert
 	console.log("CONNECT", client.string());
 }
 
+// When a client disconnects, print in console
 server.onDisconnect = function(client) {
 	console.log("DISCONNECT", client.string());
 }
 
+// Called every frame (60 times a second)
 server.gameLogic = function() {
-	posns = [];
+	// Store a bunch of info about all connected players in an array
+	players = [];
 	server.clients.forEach(function(client) {
-		posns.push(client.mousePos);
+		players.push({
+			"color": client.color.formatHEX(),
+			"position": client.tankpos,
+			"id": client.id,
+			"angle": client.angle
+		});
 	});
-	server.broadcastTrigger("set_positions", posns);
+	// Send array to every connected client, through 'update' function
+	server.broadcastTrigger("update", players);
 }
 
+// Called every frame, for each client connected
 server.clientLogic = function(client) {
-	if (client.keyPressed(Elemental.Keycodes.SPACE)) {
-		console.log("SPACE");
-	}
+	// Get movement into a vector
+	var movement = Elemental.Vector.Empty;
+	if (client.keyHeld(Elemental.Keycodes.W)) movement.y -= movespeed;
+	if (client.keyHeld(Elemental.Keycodes.S)) movement.y += movespeed;
+	if (client.keyHeld(Elemental.Keycodes.A)) movement.x -= movespeed;
+	if (client.keyHeld(Elemental.Keycodes.D)) movement.x += movespeed;
+
+	// Add movement to tank position
+	client.tankpos = Elemental.Vector.Add(client.tankpos, movement);
+
+	// Calculate barrel angle from mouse position and tank position
+	client.angle = Elemental.Helpers.AngleBetween(client.mousePos, client.tankpos);
 }
 
+// Run the server
 server.start();
